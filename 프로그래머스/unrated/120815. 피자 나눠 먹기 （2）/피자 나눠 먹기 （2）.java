0e007d2383a0b5b7e78580a5c33e371592952c07
Ji-eun Yang <38836598+yje9802@users.noreply.github.com>
2023-10-20 00:03:49 +0900

    [level unrated] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        // 최소공배수 구하기
         for (int i = 1; i <= n; i++) {
             if (i * 6 % n == 0) {
                 answer = i;

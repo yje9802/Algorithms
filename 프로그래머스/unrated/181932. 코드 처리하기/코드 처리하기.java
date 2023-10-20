131f9301e9dd50aec1353c026666db4ac313608b
Ji-eun Yang <38836598+yje9802@users.noreply.github.com>
2023-10-21 02:58:40 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        
+        int idx = 0;
+        int mode = 0;
+        String ret = "";
+        while (idx < code.length()) {
+                if (code.charAt(idx) == '1') {
+                mode = (mode + 1) % 2;
+            } else {
+                if (mode == 0) {
+                    if (idx % 2 == 0) {
+                        ret += code.charAt(idx);
+                    }
+                } else {
+                    if (idx % 2 == 1) {
+                        ret += code.charAt(idx);
+                    }
+                }
+            }
+            idx++;
+        }
+        if (ret.length() == 0) {
+           answer = "EMPTY"; 
+        } else {
+            answer = ret;
+        }
+        
+        return answer;
+    }
+}

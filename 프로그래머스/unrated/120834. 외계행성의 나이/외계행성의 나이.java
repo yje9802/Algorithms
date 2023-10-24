1e60c1d8bb03fa3fc340a051931cef3e2500922b
Ji-eun Yang <38836598+yje9802@users.noreply.github.com>
2023-10-24 21:46:01 +0900

    [level unrated] Title: 외계행성의 나이, Time: 5.51 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,12 +1,18 @@
 class Solution {
     public String solution(int age) {
-        String answer = "";
+        String answer;
         String ages = ""+age;
-        String[] arr = ages.split("");
+        char[] sb = new char[ages.length()];
+//         String[] arr = ages.split("");
 
-        for (int i = 0; i < arr.length; i++) {
-            answer += ((char) ( (Integer.parseInt(arr[i]) + 97))) ;
-        }
+//         for (int i = 0; i < arr.length; i++) {
+//             answer += ((char) ( (Integer.parseInt(arr[i]) + 97))) ;
+//         }
+        for (int i = ages.length()-1; i >= 0; i--) {
+            sb[i] = (char) ((age % 10) + (int)'a');
+            age /= 10;
+        } 
+        answer = new String(sb);
         return answer;
     }
 }

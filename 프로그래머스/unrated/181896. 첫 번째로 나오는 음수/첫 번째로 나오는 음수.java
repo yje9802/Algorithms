54049d3d79ce9ae00d6904706c297f607f468561
Ji-eun Yang <38836598+yje9802@users.noreply.github.com>
2023-10-28 16:28:31 +0900

    [level unrated] Title: 첫 번째로 나오는 음수, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] < 0) {
+                answer = i;
+                break;
+            }
+            if (i == num_list.length - 1 && answer == 0) {
+                answer = -1;
+            }
+        }
+
+        return answer;
+    }
+}

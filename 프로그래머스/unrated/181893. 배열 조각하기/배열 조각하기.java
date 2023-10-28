365a1521567f8488bbcf3e3d18157916947f5dc6
Ji-eun Yang <38836598+yje9802@users.noreply.github.com>
2023-10-28 17:53:56 +0900

    [level unrated] Title: 배열 조각하기, Time: 0.03 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -3,14 +3,16 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] arr, int[] query) {
         int[] answer;
+        int start = 0;
+        int end = arr.length - 1;
         for (int i = 0; i < query.length; i++) {
             if (i % 2 == 0) {
-                arr = Arrays.copyOfRange(arr, 0, query[i]+1);
+                end = start + query[i] - 1;
             } else {
-                arr = Arrays.copyOfRange(arr, query[i], arr.length);
+                start += query[i];
             }
         }
-        answer = arr;
+        answer = Arrays.copyOfRange(arr, start, end + 2);
         return answer;
     }
 }

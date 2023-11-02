979cef47d9e384360601f311ca4bb7947ac7f490
Ji-eun Yang <38836598+yje9802@users.noreply.github.com>
2023-11-02 15:21:00 +0900

    [level unrated] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int n) {
-        int answer = 2;
-        for (int i = 1; i <= Math.sqrt(n); i++) {
-            if (i * i == n) {
-                answer = 1;
-                break;
-            }
+        int answer = 0;
+        if (n % Math.sqrt(n) == 0) {
+            answer = 1;
+        } else {
+            answer = 2;
         }
+        
         return answer;
     }
 }

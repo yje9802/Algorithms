07e44405568e8176b34b7b700605e4c3b602d6b3
Ji-eun Yang <38836598+yje9802@users.noreply.github.com>
2023-11-07 00:54:56 +0900

    [level unrated] Title: 7의 개수, Time: 6.96 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        for (int i: array) {
+            String s = "" + i;
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == '7') {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}

ab385731cac394a0b36122821f3997f77b2b369a
Ji-eun Yang <38836598+yje9802@users.noreply.github.com>
2023-11-18 22:20:04 +0900

    [level unrated] Title: 저주의 숫자 3, Time: 0.09 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -1,13 +1,18 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for (int i = 0; i < n; i++) {
-            answer++;
-            while (answer % 3 == 0 || String.valueOf(answer).contains("3")) { 
-                answer++;
+        // for (int i = 0; i < n; i++) {
+        //     answer++;
+        //     while (answer % 3 == 0 || String.valueOf(answer).contains("3")) { 
+        //         answer++;
+        //     }
+        // }
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || String.valueOf(i).contains("3")) {
+                n++;
             }
         }
-        
+        answer = n;
         return answer;
     }
 }

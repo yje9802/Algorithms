bf327070e9cef062d554dd356f9ac65753b4849c
Ji-eun Yang <38836598+yje9802@users.noreply.github.com>
2023-11-20 17:56:44 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int a, int b) {
         int answer = 0;
         if (a % 2 == 1 && b % 2 == 1) {
-            answer = (int)Math.pow(a, 2) + (int)Math.pow(b, 2);
+            answer = (a * a) + (b * b);
         } else if ((a + b) % 2 == 1) {
             answer = 2 * (a + b);
         } else if (a % 2 == 0 && b % 2 ==0) {

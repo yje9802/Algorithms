89bfed790638ae074b7d3e172d0929bed1c247ca
Ji-eun Yang <38836598+yje9802@users.noreply.github.com>
2023-12-27 02:04:04 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,6 +1,3 @@
 def solution(numbers):
-    answer = sum([i for i in range(0, 10)])
-    
-    for i in numbers:
-        answer -= i
+    answer = sum([i for i in range(0, 10)]) - sum(numbers)
     return answer

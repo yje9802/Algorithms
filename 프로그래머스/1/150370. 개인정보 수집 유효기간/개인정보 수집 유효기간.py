650fcb31ffbac628f28286696c3a2b69e5cfffa0
Ji-eun Yang <38836598+yje9802@users.noreply.github.com>
2023-12-31 00:16:50 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.32 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,7 +1,7 @@
 def solution(today, terms, privacies):
     answer = []
     
-    today = today.split(".")
+    t_year, t_month, t_day = map(int, today.split("."))
     
     # terms_dict {약관 종류 : 유효기간}
     terms_dict = {}
@@ -9,29 +9,29 @@ def solution(today, terms, privacies):
         term = terms[i].split(" ")
         terms_dict[term[0]] = term[1]
     
-    # privacies 순회
+    # privacies 확인
     for i in range(len(privacies)):
         privacy = privacies[i].split(" ") # privacy [수집된 날짜, 약관 종류]
         limit = int(terms_dict[privacy[1]]) # privacy[1]: 약관 종류 # 유효기간
-        collected_date = privacy[0].split(".")
+        year, month, day = map(int, privacy[0].split("."))
         
-        collected_date[0] = int(collected_date[0])
-        collected_date[1] = int(collected_date[1]) + limit
-        collected_date[2] = int(collected_date[2]) - 1
-        if collected_date[2] == 0:
-            collected_date[1] -= 1
-            collected_date[2] = 28
-        while collected_date[1] > 12:
-            collected_date[0] += 1
-            collected_date[1] -= 12
+        month += limit
+        day -= 1
+        if day == 0:
+            month -= 1
+            day = 28
             
-        if int(today[0]) > collected_date[0]:
+        while month > 12:
+            year += 1
+            month -= 12
+            
+        if t_year > year:
             answer.append(i+1)
-        elif int(today[0]) == collected_date[0]:
-            if int(today[1]) > collected_date[1]:
+        elif t_year == year:
+            if t_month > month:
                 answer.append(i+1)
-            elif int(today[1]) == collected_date[1]:
-                if int(today[2]) > collected_date[2]:
+            elif t_month == month:
+                if t_day > day:
                     answer.append(i+1)
         
     return answer

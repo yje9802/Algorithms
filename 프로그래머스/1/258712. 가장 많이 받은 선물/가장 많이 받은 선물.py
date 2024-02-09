1bd2903c4366f02c495e016ee3969922af5b6470
Ji-eun Yang <38836598+yje9802@users.noreply.github.com>
2024-02-10 01:18:11 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 1.81 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -0,0 +1,38 @@
+def solution(friends, gifts):
+    gifted = {} # gifted = {"friend 이름": {"선물 준 친구 이름": 이 친구에게 준 선물 개수}}
+    gift_idx = {} # 선물 지수 
+    for friend in friends:
+        gifted[friend] = {}
+        gift_idx[friend] = 0
+    
+    for gift in gifts:
+        t, f = gift.split(' ') # t: 선물을 준 사람 f: 받은 사람
+        if t in gifted:
+            if f in gifted[t]:
+                gifted[t][f] += 1
+            else:
+                gifted[t][f] = 1
+        # 선물 지수 계산
+        gift_idx[t] += 1
+        gift_idx[f] -= 1
+        
+    will_get = [0 for _ in friends] # 각자 받게될 선물 개수
+    for i in range(len(friends)):
+        curr = friends[i]
+        for j in range(i+1, len(friends)):
+            another = friends[j]
+            a = gifted[curr][another] if another in gifted[curr] else 0
+            b = gifted[another][curr] if curr in gifted[another] else 0
+            if a > b: 
+                will_get[i] += 1
+            elif a < b:
+                will_get[j] += 1
+            elif a == b:
+                ai, bi = gift_idx[curr], gift_idx[another]
+                if ai > bi:
+                    will_get[i] += 1
+                elif ai < bi:
+                    will_get[j] += 1
+    
+    answer = max(will_get)
+    return answer

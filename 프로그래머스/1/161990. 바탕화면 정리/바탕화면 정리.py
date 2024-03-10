a0cbf9768bde471c9008013af3c7f4f9d0197450
Ji-eun Yang <38836598+yje9802@users.noreply.github.com>
2024-03-10 17:44:47 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.16 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,22 +1,11 @@
 def solution(wallpaper):
     h, w = len(wallpaper), len(wallpaper[0])
-    lux, luy = -1, -1
-    rdx, rdy = 0, 0
+    x, y = [], []
+    
     for i in range(h):
-        if "#" in wallpaper[i]:
-            for j in range(w):
-                if wallpaper[i][j] == "#":
-                    if lux == -1:
-                        lux = i
-                        luy = j
-                        rdx = i + 1
-                        rdy = j + 1
-                    else:
-                        if j < luy:
-                            luy = j
-                        elif j >= rdy:
-                            rdy = j+1
-                        rdx = i + 1
-    answer = [lux, luy, rdx, rdy]
-        
+        for j in range(w):
+            if wallpaper[i][j] == "#":
+                x.append(i)
+                y.append(j)
+    answer = [min(x), min(y), max(x)+1, max(y)+1]
     return answer

1830d5074f953d3469e451cade22d35518c7e005
Ji-eun Yang <38836598+yje9802@users.noreply.github.com>
2024-03-13 01:41:50 +0900

    [level 1] Title: 소수 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,18 +1,14 @@
 from itertools import combinations
-import math
-
-def is_prime(n):
-    for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
-            return False
-    return True
 
 def solution(nums):
     answer = 0
 
     for n in combinations(nums, 3):
         num = sum(n)
-        if is_prime(num):
-            answer += 1
+        for i in range(2, num):
+            if num % i == 0:
+                break
+        else:
+             answer += 1   
 
     return answer

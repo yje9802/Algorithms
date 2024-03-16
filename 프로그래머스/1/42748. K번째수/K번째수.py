b3545b6c0b3f6eb1261674f00ace61f9b6827784
Ji-eun Yang <38836598+yje9802@users.noreply.github.com>
2024-03-17 00:43:37 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,8 +1,7 @@
 def solution(array, commands):
     answer = []
-    for command in commands:
-        new_array = array[command[0]-1:command[1]]
-        new_array.sort()
-        answer.append(new_array[command[2]-1])
+    for c in commands:
+        new_array = sorted(array[c[0]-1:c[1]])
+        answer.append(new_array[c[2]-1])
         
     return answer

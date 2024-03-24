2ff77f8deb8c423be39f5b4cc401e7cca7db8e4f
Ji-eun Yang <38836598+yje9802@users.noreply.github.com>
2024-03-25 01:38:44 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -1,7 +1,8 @@
 def solution(x):
     answer = False
     
-    add = sum([int(n) for n in str(x)])
+    # add = sum([int(n) for n in str(x)])
+    add = sum(map(int, list(str(x))))
     if x % add == 0:
         answer = True
     return answer

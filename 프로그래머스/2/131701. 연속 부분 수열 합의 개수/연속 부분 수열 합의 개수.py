b96446134826d7807f3a5cbfac709eda37a20d5e
Ji-eun Yang <jidak1165@gmail.com>
2024-08-25 22:43:41 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4435.50 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,18 +1,14 @@
 def solution(elements):
-    answer = 0
     end = len(elements)
-    
-    i = 1
     sums = []
-    while i < end:
-        temp = elements[:i]
+    for i in range(end):
+        temp = elements[:i+1]
         sums.append(sum(temp))
         for j in range(1, end):
-            idx = (j + i - 1) % end
+            idx = (j + i) % end
             temp = temp[1:]
             temp.append(elements[idx])
             sums.append(sum(temp))
-        i += 1
     
     sums.append(sum(elements))
     answer = len(set(sums))

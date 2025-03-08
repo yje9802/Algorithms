fde1ba89e31db21a7806316c2a60c923512cb6b5
Ji-eun Yang <jidak1165@gmail.com>
2025-03-08 16:05:58 +0900

    [Gold V] Title: 토마토, Time: 1180 ms, Memory: 99488 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,34 +1,39 @@
+from sys import stdin
 from collections import deque
 
-m, n = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(n)]
+# 입력 처리
+M, N = map(int, stdin.readline().split())
+matrix = [list(map(int, stdin.readline().split())) for _ in range(N)]
 
-dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1] # 방향 이동
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 def bfs(dq):
     while dq:
         x, y = dq.popleft()
-        for i in range(4): # 상하좌우
-            nx, ny = dx[i] + x, dy[i] + y
-            if 0 <= nx < n and 0 <= ny < m and matrix[nx][ny] == 0:
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            # 격자 범위를 벗어나지 않으면서, 익지 않은 토마토인 경우
+            if 0 <= nx < N and 0 <= ny < M and matrix[nx][ny] == 0:
                 matrix[nx][ny] = matrix[x][y] + 1
-                dq.append([nx, ny])
+                dq.append((nx, ny))
+
+def get_ripe_date():
+    days = 0
+    for line in matrix:
+        for tomato in line:
+            if tomato == 0:
+                return 0
+        days = max(days, max(line))
+    return days
 
 dq = deque()
 
-for i in range(n):
-    for j in range(m):
+for i in range(N):
+    for j in range(M):
         if matrix[i][j] == 1:
-            dq.append([i, j]) # 토마토 위치를 queue에 append
+            dq.append((i, j))
 
 bfs(dq)
 
-res = 0
-for i in matrix:
-    for j in i:
-        if j == 0:
-            print(-1) # 토마토가 모두 익지 않은 상황
-            exit(0)
-    res = max(res, max(i))
-
-print(res - 1)
+print(get_ripe_date() - 1)

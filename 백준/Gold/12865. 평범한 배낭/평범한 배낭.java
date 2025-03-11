e38a45b35d6ade1ba8a75bfc6bf43723e79988cd
Ji-eun Yang <jidak1165@gmail.com>
2025-03-12 03:32:30 +0900

    [Gold V] Title: 평범한 배낭, Time: 140 ms, Memory: 15572 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[K + 1];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+
+            for (int w = K; w >= weight; w--) {
+                dp[w] = Math.max(dp[w], dp[w - weight] + value);
+            }
+        }
+
+        System.out.println(dp[K]);
+    }
+}

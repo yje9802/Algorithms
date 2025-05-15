3b4f6321c486ba414783766f2c14b6549259bc6f
Ji-eun Yang <jidak1165@gmail.com>
2025-05-15 16:43:53 +0900

    [level 2] Title: 전화번호 목록, Time: 494.86 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,19 +2,12 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Set<String> phoneSet = new HashSet<>();
-        for (String number: phone_book) {
-            phoneSet.add(number);
-        }
+        Arrays.sort(phone_book);
         
-        for (String number: phone_book) {
-            StringBuilder temp = new StringBuilder();
-            for (int i = 0; i < number.length()-1; i++) {
-                temp.append(number.charAt(i));
-                if (phoneSet.contains(temp.toString())) {
-                    return false;
-                }
-            } 
+        for (int i = 0; i < phone_book.length-1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                return false;
+            }
         }
         
         return true;

a5c9ff0378b3345fb87c14de2409bb8e4ecd637d
Ji-eun Yang <jidak1165@gmail.com>
2025-05-22 17:36:32 +0900

    [level 3] Title: 네트워크, Time: 0.47 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
 
 ### 성능 요약
 
-메모리: 82.3 MB, 시간: 0.41 ms
+메모리: 9.21 MB, 시간: 0.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 22일 17:29:53
+2025년 05월 22일 17:36:30
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,26 @@
+from collections import deque
+
 def solution(n, computers):
     answer = 0
     visited = [False for _ in range(n)]
     
-    def dfs(v):
+    def bfs(v):
         visited[v] = True # 방문처리
         
-        for node_n in range(n):
-            # 아직 방문하지 않은 노드면서 v 노드와 연결되어 있을 때
-            if not visited[node_n] and computers[v][node_n] == 1:
-                dfs(node_n)
+        dq = deque()
+        dq.append(v)
+        
+        while dq:
+            curr = dq.popleft()
+            
+            for i in range(n):
+                if visited[i] == False and computers[curr][i] == 1:
+                    visited[i] = True
+                    dq.append(i)
     
     for v in range(n): # 시작지점이 정해져 있지 않아 모든 노드를 다 검사해야 함
         if not visited[v]:
-            dfs(v)
+            bfs(v)
             answer += 1
     
     return answer

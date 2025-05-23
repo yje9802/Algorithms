77ec45edb8dcc421d64896f1eee7ccbe5e05f86f
Ji-eun Yang <jidak1165@gmail.com>
2025-05-24 03:30:24 +0900

    [level 2] Title: 구명보트, Time: 8.68 ms, Memory: 9.63 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,17 @@
+def solution(people, limit):
+    answer = 0
+    
+    people.sort()
+    
+    left, right = 0, len(people)-1
+    
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            answer += 1
+            left += 1
+            right -= 1
+        else:
+            answer += 1
+            right -= 1
+        
+    return answer

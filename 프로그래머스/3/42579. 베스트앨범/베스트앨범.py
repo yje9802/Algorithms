c5c252abd1382b699c8fb5534589bb904699db83
Ji-eun Yang <jidak1165@gmail.com>
2025-07-01 18:56:10 +0900

    [level 3] Title: 베스트앨범, Time: 0.09 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -4,28 +4,21 @@ from collections import defaultdict
 def solution(genres, plays):
     answer = []
     
-    genre_count = {} # 장르명: 총 재생 횟수
+    genre_count = defaultdict(int) # 장르명: 총 재생 횟수
     genre_plays = defaultdict(list) # 장르명: [(-재생 횟수, 고유번호), ...]
     for i in range(len(plays)):
-        if genres[i] in genre_count:
-            genre_count[genres[i]] += plays[i]
-        else:
-            genre_count[genres[i]] = plays[i]
+        genre_count[genres[i]] += plays[i]
         genre_plays[genres[i]].append((-plays[i], i))
     
-    genre_count_order = []
-    heapq.heapify(genre_count_order)
-    for k, v in genre_count.items():
-        counts = (-v, k)
-        heapq.heappush(genre_count_order, counts)
+    # 많이 재생된 순으로 장르 정렬
+    genre_count_order = sorted(genre_count.items(), key=lambda x: -x[1])
     
-    while genre_count_order:
-        _, genre = heapq.heappop(genre_count_order)
+    for genre, _ in genre_count_order:
         g_plays = genre_plays[genre]
         heapq.heapify(g_plays)
         for _ in range(2):
             if g_plays:
-                _, play = heapq.heappop(g_plays)
-                answer.append(play)
+                _, idx = heapq.heappop(g_plays)
+                answer.append(idx)
     
     return answer

9b880150df16b1b062c8c488d0cbf4b02a34eb85
Ji-eun Yang <jidak1165@gmail.com>
2025-09-01 18:43:13 +0900

    [level 3] Title: 합승 택시 요금, Time: 28.34 ms, Memory: 15.2 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/README.md
+++ 프로그래머스/3/72413. 합승 택시 요금/README.md
@@ -1,10 +1,10 @@
 # [level 3] 합승 택시 요금 - 72413 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413?language=python3) 
 
 ### 성능 요약
 
-메모리: 70.9 MB, 시간: 37.61 ms
+메모리: 15.2 MB, 시간: 28.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 01일 18:36:33
+2025년 09월 01일 18:43:11
 
 ### 문제 설명
 
--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,17 +1,17 @@
 from heapq import heappop, heappush
 
-INF = int(1e9)
+INF = int(1e9) # 무한대 설정
 graph = [[]] # graph[i] : [[i번 노드와 연결된 노드, 비용], []]
 
-def dijkstra(n, src, dest):
+def dijkstra(n, src):
     global graph
     
-    dist = [INF for _ in range(n+1)] # 시작 정점과의 거리를 저장하는 배열
-    dist[src] = 0 # 시작 정점의 거리는 0
-    pq = [[0, src]] # [시작 정점과의 거리, 노드]
+    dist = [INF for _ in range(n+1)] # src와의 거리를 저장하는 배열
+    dist[src] = 0 # src와 src 사이의 거리는 0
+    pq = [[0, src]] # pq[i] : [src와의 거리, 특정 노드]
     
     while pq:
-        w, x = heappop(pq) # w는 거리, x는 노드
+        w, x = heappop(pq) # w는 src와의 최단 거리, x는 해당 노드
         
         if dist[x] < w:
             continue
@@ -24,21 +24,27 @@ def dijkstra(n, src, dest):
                 dist[nx] = ncost
                 heappush(pq, [ncost, nx])
                 
-    return dist[dest]
+    return dist
 
 
 def solution(n, s, a, b, fares):
     global graph
     graph = [[] for _ in range(n+1)]
-    
+    # graph 설정
     for fare in fares:
         src, dest, each_cost = fare
         graph[src].append([dest, each_cost])
         graph[dest].append([src, each_cost])
     
-    answer = dijkstra(n, s, a) + dijkstra(n, s, b)
+    answer = INF
+    
+    dist_S = dijkstra(n, s)
+    dist_A = dijkstra(n, a)
+    dist_B = dijkstra(n, b)
+    
     for i in range(1, n+1):
-        if s != i: # 출발점 아님
-            answer = min(answer, dijkstra(n, s, i) + dijkstra(n, i, a) + dijkstra(n, i, b))
+        if dist_S[i] == INF or dist_A[i] == INF or dist_B[i] == INF:
+            continue
+        answer = min(answer, dist_S[i] + dist_A[i] + dist_B[i])
         
     return answer

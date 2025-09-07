a484bc3783e606e610e99297c9ada5bdedd423b8
Ji-eun Yang <jidak1165@gmail.com>
2025-09-08 01:23:50 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 1.78 ms, Memory: 9.68 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -8,6 +8,6 @@ def solution(orders, course):
         order_combs = [] # 메뉴 구성이 course_len 개인 조합 목록
         for order in orders: # 각 주문 별로 course_len 크기의 조합 계산
             order_combs += combinations(sorted(order), course_len)
-        most_ordered = Counter(order_combs).most_common()
-        answer += [comb for comb, cnt in most_ordered if cnt > 1 and cnt == most_ordered[0][1]]
-    return [''.join(s) for s in sorted(answer)]
+        most_ordered = Counter(order_combs).most_common() # 주문 횟수 기준 내림차순 정렬
+        answer += [''.join(comb) for comb, cnt in most_ordered if cnt > 1 and cnt == most_ordered[0][1]]
+    return sorted(answer)

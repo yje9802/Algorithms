87bf2345a9d748a034761a0bcf2dd4cc8838284d
Ji-eun Yang <jidak1165@gmail.com>
2026-02-09 02:09:32 +0900

    [level 2] Title: 호텔 대실, Time: 1.91 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,30 +1,21 @@
-import heapq
-
-def convert_time(time_list):
+def convert_time(time_list): # 문자열"hour:minute"을 분 단위로 변환
     hour, minute = time_list.split(":")
     return int(hour) * 60 + int(minute)
 
 def solution(book_time):
     answer = 0
     
-    times = []
-    for time in book_time:
-        start, end = time
-        heapq.heappush(times, [convert_time(start), convert_time(end) + 10])
+    rooms = [0 for _ in range(24*60 + 10)]
     
-    temp = []
-    
-    # 첫번째 방은 무조건 필요
-    _, end = heapq.heappop(times)
-    heapq.heappush(temp, end)
-    answer += 1
-    
-    while times:
-        start, end = heapq.heappop(times)
-        earlist = heapq.heappop(temp)
-        heapq.heappush(temp, end)
-        if earlist > start:
-            heapq.heappush(temp, earlist)
-            answer += 1
+    for booked in book_time:
+        start = convert_time(booked[0])
+        end = convert_time(booked[1]) + 10
         
+        rooms[start] += 1
+        rooms[end] -= 1
+    
+    # 누적합 구하기
+    for i in range(1, len(rooms)):
+        rooms[i] = rooms[i-1] + rooms[i]
+    answer = max(rooms)
     return answer

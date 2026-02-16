7666a333ca8ddb3f3143ce2ae8fe2b2b3fbda7ab
Ji-eun Yang <jidak1165@gmail.com>
2026-02-16 23:14:49 +0900

    [level 2] Title: 3 x n 타일링, Time: 24.97 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -0,0 +1,24 @@
+class Solution {
+    long MOD = 1000000007;
+    
+    public long solution(int n) {
+        long answer = 0;
+        
+        if (n % 2 == 1) return answer;
+        
+        long[] dp = new long[n+1];
+        dp[2] = 3; // 최소 단위
+
+        for (int i = 4; i < n+1; i+=2) {
+            dp[i] = dp[i-2] * 3 + 2;
+            
+            for (int j = i - 4; j > 0; j-=2) {
+                dp[i] += dp[j] * 2;
+            }
+            dp[i] = dp[i] % MOD;
+        }
+        
+        answer = dp[n];
+        return answer;
+    }
+}
--- 프로그래머스/2/12902. 3 x n 타일링/README.md
+++ 프로그래머스/2/12902. 3 x n 타일링/README.md
@@ -1,10 +1,10 @@
 # [level 2] 3 x n 타일링 - 12902 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12902?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12902?language=java) 
 
 ### 성능 요약
 
-메모리: 9.27 MB, 시간: 336.30 ms
+메모리: 76 MB, 시간: 24.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 16일 23:02:12
+2026년 02월 16일 23:14:47
 
 ### 문제 설명
 
